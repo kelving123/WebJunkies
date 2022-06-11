a9cd9a6d0242bf1cb2202be89f324a0b56ae3e2c
Alfredoxrock <alf_baza@hotmail.com>
2022-06-11 13:30:03 -0600

    Client changes

--- server/index.js
+++ server/index.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.use(express.json());
 
+//This is the connection to a Mongo data base insert connection between the quotes.
+mongoose.connect("", {
+    useNewUrlParser: true,
+});
+
 app.listen(3001, () => {
     console.log('Server running on port 3001')
 })
